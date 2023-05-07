2789ffc3b391f78ea959beb6c3f2ffea12f16f18
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-05-07 19:46:43 +0200

    3 files modified

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Import stylesheets
 import './style.css';
+
+const plusBtn = document.getElementById("plus-btn");
+
+plusBtn.addEventListener("click", openSubmitForm)
+  function openSubmitForm() {
+    console.log("click")
+  }
