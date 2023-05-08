8895b3c0938693c02ef4d25f9cf0fa0f3f388dfb
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-05-08 20:00:09 +0200

    3 files modified

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const mapBtn = document.getElementById("map-btn");
 const guideBtn = document.getElementById("guide-btn");
 const menuBtn = document.getElementById("menu-btn");
 const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn")
 
-plusBtn.addEventListener("click", openSubmitForm)
-  function openSubmitForm() {
-    console.log("click")
-    modal.style.display = "inline"
-  }
+
 
 mapBtn.addEventListener("click", openMap)
   function openMap() {
@@ -26,4 +23,14 @@ guideBtn.addEventListener("click", openGuide)
 menuBtn.addEventListener("click", openMenu)
   function openMenu() {
     console.log("click")
+  }
+
+plusBtn.addEventListener("click", openSubmitForm)
+  function openSubmitForm() {
+    modal.style.display = "inline"
+  }
+
+modalCloseBtn.addEventListener("click", closeModal)
+  function closeModal() {
+    modal.style.display = "none"
   }
