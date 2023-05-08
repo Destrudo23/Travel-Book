ceb742187107d13641483066b26474d959f966a4
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-05-08 18:53:01 +0200

    3 files modified

--- index.js
+++ index.js
@@ -2,8 +2,28 @@
 import './style.css';
 
 const plusBtn = document.getElementById("plus-btn");
+const mapBtn = document.getElementById("map-btn");
+const guideBtn = document.getElementById("guide-btn");
+const menuBtn = document.getElementById("menu-btn");
+const modal = document.getElementById("modal");
 
 plusBtn.addEventListener("click", openSubmitForm)
   function openSubmitForm() {
+    console.log("click")
+    modal.style.display = "inline"
+  }
+
+mapBtn.addEventListener("click", openMap)
+  function openMap() {
+    console.log("click")
+  }
+
+guideBtn.addEventListener("click", openGuide)
+  function openGuide() {
+    console.log("click")
+  }
+
+menuBtn.addEventListener("click", openMenu)
+  function openMenu() {
     console.log("click")
   }
